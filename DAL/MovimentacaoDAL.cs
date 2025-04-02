2bee3a43542d8cdc4a8fa63d99f33452904cc7ae
Jansen Alexandre <jansen3lenxadre@gmail.com>
2025-04-02 19:03:42 -0300

    Base da função(Movimentações)

---
+++ DAL/MovimentacaoDAL.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Estoque.DAL
+{
+    public class MovimentacaoDAL
+    {
+
+        private readonly string strConnection = "server=localhost;port=3306;User Id=root;database=estoquedb;password=J#nsen1804";
+
+        public void EntradaEstoque(int produtoID, int quantidade, int fornecedorID)
+        {
+            using (MySqlConnection con = new MySqlConnection(strConnection))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand("EntradaEstoque", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@p_ProdutoID", produtoID);
+                    cmd.Parameters.AddWithValue("@p_Quantidade", quantidade);
+                    cmd.Parameters.AddWithValue("@p_FornecedorID", fornecedorID);
+                    
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void SaidaEstoque(int produtoID, int quantidade, string motivo)
+        {
+            using (MySqlConnection con = new MySqlConnection(strConnection))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand("EntradaEstoque", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@p_ProdutoID", produtoID);
+                    cmd.Parameters.AddWithValue("@p_Quantidade", quantidade);
+                    cmd.Parameters.AddWithValue("@p_Motivo", motivo);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw new Exception("Erro ao registrar saída de estoque: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Entities/MovimentacaoEstoque.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Estoque.Entities
+{
+    public class MovimentacaoEstoque
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataMovimentacao { get; set; }
+        public int UsuarioId { get; set; }
+        public int? FornecedorId { get; set; }
+    }
+}
